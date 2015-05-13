ac71afb581708f71943b2fb6fcc6fdf94e6046dd
Jayant Srivastava <sjayant@gmail.com>
2015-05-12 21:36:06 -0700

    Added Core with Logging functionality

---
+++ src/frauddetect/common/core/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("frauddetect.common.core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("frauddetect.io")]
+[assembly: AssemblyProduct("frauddetect")]
+[assembly: AssemblyCopyright("Copyright © frauddetect.io 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5DC5E9C7-7975-42FE-97BA-2038A8F8F5DE")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/frauddetect/common/core/logging/Logger.cs
@@ -0,0 +1,60 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frauddetect.common.core.logging
+{
+    sealed class Logger
+    {
+        private bool IsInitialized;
+        private Object Lock = new Object();
+
+        public void Initialize(string applicationName, string configFilePath)
+        {
+            if(string.IsNullOrWhiteSpace(applicationName))
+            {
+                throw new Exception("Application name is empty.");
+            }
+
+            if(string.IsNullOrWhiteSpace(configFilePath))
+            {
+                throw new Exception("Configuration file path is empty.");
+            }
+
+            if(!File.Exists(configFilePath))
+            {
+                throw new Exception("Configuration file doesn't exist.");
+            }
+
+            if (IsInitialized)
+            {
+                return;
+            }
+
+            lock (Lock)
+            {
+                XmlConfigurator.Configure();
+                GlobalContext.Properties["ApplicationName"] = applicationName;
+
+                XmlConfigurator.ConfigureAndWatch(new FileInfo(configFilePath));
+
+                IsInitialized = true;
+            }
+        }
+
+        public ILog GetLogger(Type T)
+        {
+            if (IsInitialized == false)
+            {
+                throw new Exception("Logger isn't initialized.");
+            }
+
+            return LogManager.GetLogger(T);
+        }
+    }
+}

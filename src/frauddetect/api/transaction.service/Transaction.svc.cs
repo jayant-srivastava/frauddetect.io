ac40a4ef6dfacb87f80afd1a52056a2ccbf7e02d
Jayant Srivastava <sjayant@gmail.com>
2015-05-20 18:56:03 -0700

    Added logger null pointer checks

--- src/frauddetect/api/transaction.service/Transaction.svc.cs
+++ src/frauddetect/api/transaction.service/Transaction.svc.cs
@@ -1,6 +1,7 @@
 ﻿using frauddetect.common.core.logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -15,7 +16,7 @@ namespace transaction.service
 
         private readonly log4net.ILog _logger;
         private const string APPLICATIONNAME = "transaction.service";
-        private const string LOGCONFIG = @"C:\Projects\frauddetect.io\deployment\configuration\logging.config";
+        private const string LOGCONFIG = @"C:\frauddetect.io\support\configuration\logging.config";
         private readonly bool _initialized = false;
 
         #endregion
@@ -42,40 +43,74 @@ namespace transaction.service
 
         public TransactionOutput Authorize(TransactionInput transactionInput)
         {
-            _logger.Debug("START: Authorize()");
+            if (_logger != null)
+            {
+                _logger.Debug("START: Authorize()");
+            }
 
             try
             {
-                _logger.Debug(string.Format("Account: {0}, Ammount: {1}, Store: {2}", transactionInput.Account, transactionInput.Amount, transactionInput.Store));
+                if(!_initialized)
+                {
+                    throw new Exception("Service isn't initialized.");
+                }
+
+                if (_logger != null)
+                {
+                    _logger.Debug(string.Format("Account: {0}, Ammount: {1}, Store: {2}", transactionInput.Account, transactionInput.Amount, transactionInput.Store));
+                }
+
                 return new TransactionOutput() { Success = true, AuthorizationCode = "100", StatusCode = 0, Message = string.Empty };
             }
             catch (Exception ex)
             {
-                _logger.Error("EXCEPTION: Authorize()", ex);
+                if (_logger != null)
+                {
+                    _logger.Error("EXCEPTION: Authorize()", ex);
+                }
+
                 return new TransactionOutput() { Success = false, AuthorizationCode = string.Empty, StatusCode = -1, Message = ex.Message };
             }
             finally
             {
-                _logger.Debug("END: Authorize()");
+                if (_logger != null)
+                {
+                    _logger.Debug("END: Authorize()");
+                }
             }
         }
 
         public bool Health()
         {
-            _logger.Debug("START: Health()");
+            if (_logger != null)
+            {
+                _logger.Debug("START: Health()");
+            }
 
             try
             {
+                if (!_initialized)
+                {
+                    throw new Exception("Service isn't initialized.");
+                }
+
                 return true;
             }
             catch (Exception ex)
             {
-                _logger.Error("EXCEPTION: Health()", ex);
+                if (_logger != null)
+                {
+                    _logger.Error("EXCEPTION: Health()", ex);
+                }
+
                 return false;
             }
             finally
             {
-                _logger.Debug("END: Health()");
+                if (_logger != null)
+                {
+                    _logger.Debug("END: Health()");
+                }
             }
         }
 

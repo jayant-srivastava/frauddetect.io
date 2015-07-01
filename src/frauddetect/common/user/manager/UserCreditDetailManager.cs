eaa8ec629d615f09b60cd4150ae1a17e21d70c46
Jayant Srivastava <sjayant@gmail.com>
2015-06-30 19:35:46 -0700

    Add User Credit Details manager functions

--- src/frauddetect/common/user/manager/UserCreditDetailManager.cs
+++ src/frauddetect/common/user/manager/UserCreditDetailManager.cs
@@ -47,6 +47,33 @@ namespace frauddetect.common.user.manager
             return;
         }
 
+        public void Update(UserCreditDetail userCreditDetail)
+        {
+            IsInitialized();
+
+            if (userCreditDetail == null) { throw new ArgumentNullException("User credit detail object is null."); }
+            if (string.IsNullOrWhiteSpace(userCreditDetail.Account)) { throw new ArgumentNullException("Account number is blank."); }
+
+            long count = UserCreditDetailsCollection.Find(Query<UserCreditDetail>.EQ(u => u.Account, userCreditDetail.Account)).Count(); 
+            if (count == 0) { throw new Exception("Account doesn't exist."); }
+
+            WriteConcernResult result = UserCreditDetailsCollection.Save(userCreditDetail);
+            if (result != null && !result.Ok) { throw new Exception("Failed to update user credit detail record."); }
+        }
+
+        public void Delete(UserCreditDetail userCreditDetail)
+        {
+            IsInitialized();
+
+            if (userCreditDetail == null) { throw new ArgumentNullException("User credit detail object is null."); }
+            if (string.IsNullOrWhiteSpace(userCreditDetail.Account)) { throw new ArgumentNullException("Account number is blank."); }
+
+            WriteConcernResult result = UserCreditDetailsCollection.Remove(Query<UserCreditDetail>.EQ(u => u.Account, userCreditDetail.Account));
+            if (result != null && !result.Ok) { throw new Exception("Failed to delete user credit detail record."); }
+
+            return;
+        }
+
         public UserCreditDetail GetByAccount(string account)
         {
             IsInitialized();

6fa75914ce6914e5b3c09722a5aefaacbb72c30b
Jayant Srivastava <sjayant@gmail.com>
2015-06-30 20:21:58 -0700

    Fraud Service

---
+++ src/frauddetect/api/fraud.service/Fraud.svc.cs
@@ -0,0 +1,122 @@
+﻿using Splunk;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading;
+
+namespace frauddetect.api.fraud.service
+{
+    public class Fraud : IFraud
+    {
+        public List<FraudOutput> FraudDetails()
+        {
+            Service splunkService = null;
+
+            try
+            {
+                #region Connect to Splunk Server
+
+                ServiceArgs serviceArgs = new ServiceArgs()
+                {
+                    Host = ConfigurationManager.AppSettings["SPLUNKSERVER"],
+                    Port = int.Parse(ConfigurationManager.AppSettings["SPLUNKPORT"]),
+                };
+
+                splunkService = new Service(serviceArgs);
+                splunkService.Login(ConfigurationManager.AppSettings["SPLUNKUSER"], ConfigurationManager.AppSettings["SPLUNKPASSWORD"]);
+
+                #endregion
+
+                #region Create Job parameters
+
+                JobArgs jobArgs = new JobArgs()
+                {
+                    EarliestTime = "2015-06-014T12:00:00.000-07:00",
+                    LatestTime = "2015-06-20T12:00:00.000-07:00",
+                };
+
+                jobArgs.ExecutionMode = JobArgs.ExecutionModeEnum.Normal;
+                string query = "search * | head 50000 | search source=\"C:\\\\frauddetect.io\\\\support\\\\logs\\\\fraud.server.log\" | fields amount,latitude,longitude";
+
+                #endregion
+
+                #region Create Splunk Search Job and wait for it to finish
+
+                Job job = splunkService.GetJobs().Create(query, jobArgs);
+                while (!job.IsDone)
+                {
+                    try
+                    {
+                        Thread.Sleep(500);
+                    }
+                    catch (ThreadInterruptedException e)
+                    {
+                        throw new Exception("Job failed.", e.InnerException);
+                    }
+                }
+
+                #endregion
+
+                #region Parse the results returned by Splunk
+
+                var outArgs = new JobResultsArgs
+                {
+                    OutputMode = JobResultsArgs.OutputModeEnum.Xml,
+                    Count = 0,
+                };
+
+                List<FraudOutput> output = new List<FraudOutput>();
+                using (var stream = job.Results(outArgs))
+                {
+                    using (var rr = new ResultsReaderXml(stream))
+                    {
+                        foreach (var @event in rr)
+                        {
+                            FraudOutput fraudOutput = new FraudOutput();
+
+                            foreach (string key in @event.Keys)
+                            {
+                                switch (key)
+                                {
+                                    case "amount":
+                                        fraudOutput.Amount = double.Parse(@event[key]);
+                                        break;
+                                    case "longitude":
+                                        fraudOutput.Longitude = double.Parse(@event[key]);
+                                        break;
+                                    case "latitude":
+                                        fraudOutput.Latitude = double.Parse(@event[key]);
+                                        break;
+                                }
+                            }
+
+                            output.Add(fraudOutput);
+                        }
+                    }
+                }
+
+                #endregion
+
+                return output;
+            }
+            catch(Exception ex)
+            {
+                //log exception
+                return new List<FraudOutput>();
+            }
+            finally
+            {
+                if(splunkService != null)
+                {
+                    splunkService.Logout();
+                }
+            }
+        }
+    }
+}
---
+++ src/frauddetect/api/fraud.service/IFraud.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace frauddetect.api.fraud.service
+{
+    [ServiceContract]
+    public interface IFraud
+    {
+        [WebGet(UriTemplate = "/fraud", BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        List<FraudOutput> FraudDetails();  
+    }
+
+    [DataContract]
+    [Serializable]
+    public class FraudOutput
+    {
+        public FraudOutput() {}
+
+        public FraudOutput(double latitude, double longitude, double amount)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            Amount = amount;
+        }
+
+        [DataMember]
+        public double Latitude { get; set; }
+
+        [DataMember]
+        public double Longitude { get; set; }
+
+        [DataMember]
+        public double Amount { get; set; }
+    }
+}
